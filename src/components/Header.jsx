4d57a6f555091080f8769a60abfe7d1eca5134bf
GustavoBrcz <87142619+GustavoBrcz@users.noreply.github.com>
2021-12-14 14:53:49 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,6 @@ import React from "react";
 
 function Header ({titulo}) {
 
-
-    // const nombre = "Gustavo";
-
-    // var mensaje;
-
-    // if (nombre === "Gustavo") {
-    //     mensaje = "Eres el mejor del mundo";
-    // }else {
-    //     mensaje = "No eres Gustavo";
-    // }
-
-    // const edad = 25;
-
-
-    // var notificacion = (edad >= 18) ? "Eres mayor de edad" : "No estás apto para estar aquí";
-
     return (
         <h1 id = "titulo" className="encabezado">{titulo}</h1>
     );
